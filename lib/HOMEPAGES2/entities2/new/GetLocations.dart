2e2101e05dc9000d3200784ad6db034938ce01c4
JohnRaves-Ranara <jrRanara@mcm.edu.ph>
2024-02-17 08:05:17 +0800

    added location class and getlocation. Locations fetched from geojson data."

---
+++ lib/HOMEPAGES2/entities2/new/GetLocations.dart
@@ -0,0 +1,37 @@
+
+import 'dart:io';
+import 'dart:convert';
+import 'Location.dart';
+
+class GetLocations{
+
+  List<Location> getLocations(){
+  String geojsonData = File('lib/MuniCities.minimal.json').readAsStringSync();
+
+  // Parse JSON
+  Map<String, dynamic> jsonData = json.decode(geojsonData);
+
+  // Extract "features" array
+  List<dynamic> features = jsonData['features'];
+
+  // Map to List<Location>
+  List<Location> locations = features.map((feature) {
+    return Location(
+      provID: feature['properties']['ID_1'],
+      munID: feature['properties']['ID_2'],
+      provName: feature['properties']['NAME_1'],
+      munName: feature['properties']['NAME_2'],
+    );
+  }).toList();
+
+  return locations;
+    
+  }
+}
+
+// void main(){
+//   final locations = GetLocations().getLocations();
+//   locations.forEach((location) {
+//     print('provID: ${location.provID}, munID: ${location.munID}, provName: ${location.provName}, munName: ${location.munName}');
+//   });
+// }
